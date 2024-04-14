72935ec7b1d45c5700a4d9a3a440a168d15e2a1d
MoetaYuko <loli@yuko.moe>
2024-04-14 19:17:23 +0800

    fbdev: Align deferred I/O with naming of helpers

--- backport-include/linux/fb.h
+++ backport-include/linux/fb.h
@@ -4,5 +4,10 @@
 #ifdef BPM_FB_ACTIVATE_KD_TEXT_NOT_PRESENT
 #define FB_ACTIVATE_KD_TEXT   512       /* for KDSET vt ioctl */
 #endif
+
+#if LINUX_VERSION_IS_LESS(6,6,0)
+#define FB_GEN_DEFAULT_DEFERRED_IOMEM_OPS FB_GEN_DEFAULT_DEFERRED_IO_OPS
+#endif
+
 #endif
 
